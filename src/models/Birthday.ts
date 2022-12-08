6595ddff50e794a3d02f10ff2bcc043c3807d3d7
Croos3r <contact.crooser@gmail.com>
2022-12-08 19:05:41 +0100

    add: CRUD for Birthday

--- src/models/Birthday.ts
+++ src/models/Birthday.ts
@@ -1,4 +1,5 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+import database from '../utils/database.js'
 
 @Entity()
 export default class Birthday {
@@ -13,4 +14,23 @@ export default class Birthday {
 
   @Column({ nullable: true, type: 'date', default: null })
   lastNotified!: Date | null
+}
+
+export const birthdayRepository = database.getRepository(Birthday)
+
+export async function getBirthdayForUser(userId: string) {
+  return await birthdayRepository.find({ where: { userId } })
+}
+
+export async function addBirthdayForUser(userId: string, date: Date) {
+  const birthday = birthdayRepository.create({ userId, date })
+  await birthdayRepository.save(birthday)
+}
+
+export async function removeBirthdayForUser(userId: string) {
+  await birthdayRepository.delete({ userId })
+}
+
+export async function setBirthdayLastNotifiedTodayForUser(userId: string) {
+  await birthdayRepository.update({ userId }, { userId, lastNotified: new Date() })
 }
