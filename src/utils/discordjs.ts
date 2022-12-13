e6e3bced24032e5f44f39c81abc2ca7909c25759
Crooser <54242096+Croos3r@users.noreply.github.com>
2022-12-14 00:53:48 +0100

    del: useless ephemeral predict, will reimplement when needed as it's fairly easy

--- src/utils/discordjs.ts
+++ src/utils/discordjs.ts
@@ -7,43 +7,35 @@ import {
 } from 'discord.js'
 
 type RepliableInteraction =
-    | CommandInteraction
-    | MessageComponentInteraction
-    | ModalSubmitInteraction
+  | CommandInteraction
+  | MessageComponentInteraction
+  | ModalSubmitInteraction
 
 export class InteractionReplier {
-    private nextIsEphemeral: boolean
+  private nextIsEphemeral: boolean
 
-    constructor(
-        private interaction: RepliableInteraction,
-        private ephemeral: boolean,
-    ) {
-        this.nextIsEphemeral = this.ephemeral
-    }
+  constructor(
+    private interaction: RepliableInteraction,
+    private ephemeral: boolean,
+  ) {
+    this.nextIsEphemeral = this.ephemeral
+  }
 
-    async replyMessage(message: string, options?: Omit<InteractionReplyOptions, 'content' | 'ephemeral'>) {
-        const ephemeral = this.nextIsEphemeral || this.ephemeral
-        await this.interaction.reply({content: message, ephemeral: ephemeral, ...options})
-        this.nextIsEphemeral = this.ephemeral
-    }
+  async replyMessage(message: string, options?: Omit<InteractionReplyOptions, 'content' | 'ephemeral'>) {
+    const ephemeral = this.nextIsEphemeral || this.ephemeral
+    await this.interaction.reply({ content: message, ephemeral: ephemeral, ...options })
+    this.nextIsEphemeral = this.ephemeral
+  }
 
-    async replyEmbed(embed: APIEmbed, options?: Omit<InteractionReplyOptions, 'embeds' | 'ephemeral'>) {
-        const ephemeral = this.nextIsEphemeral || this.ephemeral
-        await this.interaction.reply({embeds: [embed], ephemeral: ephemeral, ...options})
-        this.nextIsEphemeral = this.ephemeral
-    }
+  async replyEmbed(embed: APIEmbed, options?: Omit<InteractionReplyOptions, 'embeds' | 'ephemeral'>) {
+    const ephemeral = this.nextIsEphemeral || this.ephemeral
+    await this.interaction.reply({ embeds: [ embed ], ephemeral: ephemeral, ...options })
+    this.nextIsEphemeral = this.ephemeral
+  }
 
-    async replyEmbeds(embeds: APIEmbed[], options?: Omit<InteractionReplyOptions, 'embeds' | 'ephemeral'>) {
-        const ephemeral = this.nextIsEphemeral || this.ephemeral
-        await this.interaction.reply({embeds, ephemeral: ephemeral, ...options})
-        this.nextIsEphemeral = this.ephemeral
-    }
-
-    nextEphemeral() {
-        this.nextIsEphemeral = true
-    }
-
-    nextNotEphemeral() {
-        this.nextIsEphemeral = false
-    }
+  async replyEmbeds(embeds: APIEmbed[], options?: Omit<InteractionReplyOptions, 'embeds' | 'ephemeral'>) {
+    const ephemeral = this.nextIsEphemeral || this.ephemeral
+    await this.interaction.reply({ embeds, ephemeral: ephemeral, ...options })
+    this.nextIsEphemeral = this.ephemeral
+  }
 }
