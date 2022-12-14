bce7554848ef16c67c9711166318d3dd9040f70a
Crooser <54242096+Croos3r@users.noreply.github.com>
2022-12-14 01:28:34 +0100

    add: impl of activity rotation worker

---
+++ src/utils/activity.ts
@@ -0,0 +1,27 @@
+import packageJson from '../../package.json' assert { type: 'json' };
+import { ActivitiesOptions, ActivityType } from "discord.js";
+import { Client } from 'discordx';
+
+export const VERSION_ACTIVITY: ActivitiesOptions = {
+  name: `v${packageJson.version} | by ${packageJson.author.name}`,
+  type: ActivityType.Playing,
+  url: packageJson.author.url,
+}
+
+export const CONTRIBUTE_ACTIVITY: ActivitiesOptions = {
+  name: `Contribute on GitHub -> ${packageJson.author.url}`,
+  type: ActivityType.Watching,
+};
+
+const ACTIVITIES: ActivitiesOptions[] = [
+  VERSION_ACTIVITY,
+  CONTRIBUTE_ACTIVITY,
+];
+
+export function rotateActivities(bot: Client, delay = 5, activities: ActivitiesOptions[] = ACTIVITIES): void {
+  let i = 0;
+  setInterval(() => {
+    console.log('Rotating activity to', activities[i].name);
+    bot.user?.setActivity(activities[i++ % activities.length]);
+  }, delay * 1000);
+}
