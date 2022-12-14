0b37f3a002b64daa674e7666b580bddd50185ad1
Dorian Moy <contact.crooser@gmail.com>
2022-12-14 01:04:54 +0100

    Adding ephemeral mode to birthdays and easy implementation for futures features (#7)

---
+++ src/utils/discordjs.ts
@@ -0,0 +1,41 @@
+import {
+  APIEmbed,
+  CommandInteraction,
+  InteractionReplyOptions,
+  MessageComponentInteraction,
+  ModalSubmitInteraction,
+} from 'discord.js'
+
+type RepliableInteraction =
+  | CommandInteraction
+  | MessageComponentInteraction
+  | ModalSubmitInteraction
+
+export class InteractionReplier {
+  private nextIsEphemeral: boolean
+
+  constructor(
+    private interaction: RepliableInteraction,
+    private ephemeral: boolean,
+  ) {
+    this.nextIsEphemeral = this.ephemeral
+  }
+
+  async replyMessage(message: string, options?: Omit<InteractionReplyOptions, 'content' | 'ephemeral'>) {
+    const ephemeral = this.nextIsEphemeral || this.ephemeral
+    await this.interaction.reply({ content: message, ephemeral: ephemeral, ...options })
+    this.nextIsEphemeral = this.ephemeral
+  }
+
+  async replyEmbed(embed: APIEmbed, options?: Omit<InteractionReplyOptions, 'embeds' | 'ephemeral'>) {
+    const ephemeral = this.nextIsEphemeral || this.ephemeral
+    await this.interaction.reply({ embeds: [ embed ], ephemeral: ephemeral, ...options })
+    this.nextIsEphemeral = this.ephemeral
+  }
+
+  async replyEmbeds(embeds: APIEmbed[], options?: Omit<InteractionReplyOptions, 'embeds' | 'ephemeral'>) {
+    const ephemeral = this.nextIsEphemeral || this.ephemeral
+    await this.interaction.reply({ embeds, ephemeral: ephemeral, ...options })
+    this.nextIsEphemeral = this.ephemeral
+  }
+}
